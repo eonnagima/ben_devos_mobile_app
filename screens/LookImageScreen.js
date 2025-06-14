90155281fbe91f577a174e47c6feee630634627b
Ben Devos <bendevos07@gmail.com>
2025-06-14 19:48:47 +0200

    feat: tap look to see image fullscreen

---
+++ screens/LookImageScreen.js
@@ -0,0 +1,32 @@
+// LookDetailScreen.js
+import React from 'react';
+import { View, Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+
+const LookImageScreen = ({ route, navigation }) => {
+  const { image } = route.params;
+
+  return (
+    <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+      <View style={styles.container}>
+        <Image source={image} style={styles.fullscreenImage} />
+        
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fullscreenImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
+
+export default LookImageScreen;
