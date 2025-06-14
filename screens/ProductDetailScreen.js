0844bde433e49b4d27ce346f0aea4ac4de7085d9
Ben Devos <bendevos07@gmail.com>
2025-06-14 10:57:46 +0200

    refactor: dedicated products page

---
+++ screens/ProductDetailScreen.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { BackHandler, ScrollView, StyleSheet, Text, View, Image, Button, TouchableOpacity, Touchable } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'; 
+import ProductCard from '../components/productCard';
+import * as Font from 'expo-font';
+
+const DetailScreen = ({route}) => {
+    const product = route.params;
+    const [fontsLoaded, setFontsLoaded] = useState(false); //een usestate laat ons toe om de waarde van een variabele te veranderen zonder de pagina te herladen
+    const [quantity, setQuantity] = useState(1);
+
+    const increaseQuantity = () => { setQuantity(quantity + 1); }
+    const decreaseQuantity = () => { 
+        if (quantity > 1) {
+            setQuantity(quantity - 1); 
+        }
+    }
+
+return (
+    <ScrollView style={styles.background}>
+            <View style={styles.container}>
+                <Image
+                    source={product.mainImage}
+                    style={styles.image}
+                />
+                <Text style={styles.header1}>{product.name}</Text>
+                <Text style={styles.productPrice}>€{product.price * quantity}</Text>
+            </View>
+        <View style={styles.quantity}>
+            <TouchableOpacity onPress={decreaseQuantity}>
+                <Text style={styles.productPrice}>-</Text>
+            </TouchableOpacity>
+            <Text style={styles.productPrice}>{quantity}</Text>
+            <TouchableOpacity onPress={increaseQuantity}>
+                <Text style={styles.productPrice}>+</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+            <Button
+                title="Add to cart"
+                color="#000"
+                onPress = {() => alert('Added to cart')}
+            />
+        </View>
+        <View style={styles.container}>
+            <Text style={{ fontFamily: 'Elza-Text-Medium', color: 'black', textAlign: 'center' }}>
+                {product.description}
+            </Text>
+        </View>
+    {product.otherImages && product.otherImages.length > 0 && (
+    <View style={styles.otherImagesContainer}>
+        {product.otherImages.map((img, index) => (
+        <Image
+            key={index}
+            source={img}
+            style={styles.image}
+        />
+        ))}
+    </View>
+    )}
+    </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    background:{
+        backgroundColor: '#fff'
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 40,
+      paddingHorizontal: 20
+    },
+    quantity: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20,
+        marginVertical: 20,
+
+    },
+    header1: {
+      fontSize: 32,
+      color: 'black',
+      textAlign: 'center',
+      marginTop: 20,
+      fontFamily: 'Larken-ExtraBold',
+      textTransform: 'uppercase'
+    },
+    image: {
+        width: "100%",
+        height: 200,
+        borderRadius: 4,
+        margin: 24,
+        alignSelf: "center",
+        objectFit: "contain"
+    },
+    productPrice:{
+        fontSize: 18,
+        fontFamily: 'Elza-Text-Medium',
+        color: "black"
+    }
+});
+
+export default DetailScreen;
---
+++ screens/ProductsScreen.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { BackHandler, ScrollView, StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'; 
+import ProductCard from '../components/productCard';
+import * as Font from 'expo-font';
+import{ DEMONIA_WEBFLOW_API_KEY} from '@env';
+
+const ProductsScreen = ({navigation}) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            'https://api.webflow.com/v2/sites/67ab9628a0b2d39659eb5d0f/products',
+            {
+                headers:{
+                    Authorization:
+                        `Bearer ${DEMONIA_WEBFLOW_API_KEY}`,
+                },
+            }
+        )
+            .then((res) => res.json()) //res = response
+            .then((data) => {
+                setProducts(
+                    data.items.map((item) => ({
+                        id: item.product.id,
+                        name: item.product.fieldData.name,
+                        description: item.product.fieldData.description,
+                        price: (item.skus[0]?.fieldData.price.value || 0) / 100, //convert cents to euros
+                        mainImage: {uri: item.skus[0]?.fieldData["main-image"]?.url },
+                        otherImages: (item.skus[0]?.fieldData["more-images"] || []).map(img => ({ uri: img.url })),
+                    }))
+                )
+            })
+            .catch((error) => console.error('Error fetching products:', error));
+    }, []);
+
+    return (
+        <ScrollView style={styles.background}>
+            <View style={styles.container}>
+            <Text style={styles.header1}>Shoes</Text>
+            </View>
+            <View>
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        name={product.name}
+                        price={product.price}
+                        mainImage={product.mainImage}
+                        onPress={() => navigation.navigate('ProductDetails', product)}
+                    />
+                ))}
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    background:{
+        backgroundColor: '#fff'
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 40,
+      paddingHorizontal: 20
+    },
+    header1: {
+      fontSize: 32,
+      color: 'black',
+      textAlign: 'center',
+      marginTop: 20,
+      fontFamily: 'Larken-ExtraBold',
+      textTransform: 'uppercase'
+    },
+
+});
+
+export default ProductsScreen;
